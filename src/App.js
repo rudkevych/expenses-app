dbfc9ef53862327a88e4507b82b777f702d01960
rudkevych <oksana.rudkevych@gmail.com>
2022-05-12 00:31:28 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ExpenseItem from "./components/ExpenseItem";
+import ExpenseDate from "./components/ExpenseDate";
+import Expenses from "./components/Expenses";
+
+function App() {
+    const expenses = [
+        {
+            title: 'Book',
+            amount: 45.23,
+            date: new Date(2020, 3, 5)
+        },
+        {
+            title: 'Bed',
+            amount: 250.40,
+            date: new Date(2020, 7, 2)
+        },
+        {
+            title: 'Perfume',
+            amount: 80.3,
+            date: new Date(2020, 5, 12)
+        },
+        {
+            title: 'Mcdonalds',
+            amount: 8,
+            date: new Date(2020, 3, 26)
+        }
+    ];
+    return (
+        <div className="App">
+            <Expenses expenses={expenses}/>
+        </div>
+    );
+}
+
+export default App;
