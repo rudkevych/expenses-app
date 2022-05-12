d0d3c9091212c445c4c8b16f4d968844ea518ca5
rudkevych <oksana.rudkevych@gmail.com>
2022-05-12 16:14:54 +0200

    added Card, Expenses, ExpenseItem components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
 import './ExpenseItem.css';
-import ExpenseDate from "./ExpenseDate";
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 
 function ExpenseItem(props) {
     return (
-        <div className='expense-item'>
-            <ExpenseDate />
+        <Card className='expense-item'>
+            <ExpenseDate/>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
-        </div>
+        </Card>
     )
 }
 
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,8 +1,10 @@
-import ExpenseItem from "./ExpenseItem";
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+import Card from './Card';
 
 function Expenses(props) {
     return (
-        <div>
+        <Card className='expenses'>
             <ExpenseItem
                 title={props.expenses[0].title}
                 amount={props.expenses[0].amount}
@@ -11,7 +13,7 @@ function Expenses(props) {
                 title={props.expenses[1].title}
                 amount={props.expenses[1].amount}
             />
-        </div>
+        </Card>
     )
 
 }
